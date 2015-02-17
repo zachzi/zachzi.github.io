f4b4a7579f07eb33bb933b3927af9c53dae8b7b2
Zachary Ziebarth <zachzi@live.com>
2015-02-17 15:12:52 -0800

    Update OneDrive.debug.js

--- OneDrive.debug.js
+++ OneDrive.debug.js
@@ -1,5 +1,5 @@
 //! Copyright (c) Microsoft Corporation. All rights reserved.
-// WL.JS Version 5.5.8612.2002
+// WL.JS Version 5.5.8617.2002
 
 (function() {
     if (!window.WL && !window.OneDrive) {
@@ -379,7 +379,7 @@ OneDriveApp.prototype = {
                         var vroomFolderId = folderId.split(".")[2];
                         var accessToken = internalApp.getAccessTokenForApi();
                         var urlUploadProperties = {
-                            path: "drives/" + /*pickerResponse.owner_cid*/ "-7592275166240781166"  + "/items/" + vroomFolderId + "/children",
+                            path: "drives/" + pickerResponse.owner_cid + "/items/" + vroomFolderId + "/children",
                             method: HTTP_METHOD_POST, 
                             use_vroom_api: true,
                             request_headers: [{ name: API_PARAM_PREFER, value: API_PARAM_RESPOND_ASYNC }, { name: API_PARAM_AUTH, value: "bearer " + accessToken }],
@@ -496,7 +496,7 @@ OneDriveApp.prototype = {
             );
         };
 
-        delayInvoke(pollForProgress, 100 /* milliseconds */);
+        delayInvoke(pollForProgress, 1000 /* milliseconds */);
     },
 
     validateOpenParameters: function () {
@@ -6922,7 +6922,7 @@ WLText = {
  */
 wl_app._locale = "en";
 
-        wl_app[API_X_HTTP_LIVE_LIBRARY] = "Web/DEVICE_" + trimVersionBuildNumber("5.5.8612.2002");
+        wl_app[API_X_HTTP_LIVE_LIBRARY] = "Web/DEVICE_" + trimVersionBuildNumber("5.5.8617.2002");
 
         wl_app.testInit = function(properties) {
 
@@ -6944,7 +6944,7 @@ wl_app._locale = "en";
         prodSettings[WL_APISERVICE_URI] = "https://apis.live.net/v5.0/";
         prodSettings[WL_SKYDRIVE_URI] = "https://onedrive.live.com/";
         prodSettings[WL_SDK_ROOT] = "//js.live.net/v5.0/";
-        prodSettings[WL_ONEDRIVE_API] = "https://df.api.onedrive.com/v1.0/";
+        prodSettings[WL_ONEDRIVE_API] = "https://api.onedrive.com/v1.0/";
 
         var dfSettings = {};
         dfSettings[WL_AUTH_SERVER] = "login.live.com";
@@ -6975,7 +6975,7 @@ wl_app._locale = "en";
             }
         };
 
-        wl_app._settings.init("PROD");
+        wl_app._settings.init("DF");
 
         wl_app[FILEDIALOG_PARAM_PICKER_SCRIPT] = "wl.skydrivepicker.debug.js";
         wl_app.onloadInit();
