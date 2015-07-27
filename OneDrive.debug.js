e54e4560c6200e9144147daf3b651cb2a67db8b0
Zachary Ziebarth <zachzi@live.com>
2015-07-27 16:56:25 -0700

    Update OneDrive.debug.js

--- OneDrive.debug.js
+++ OneDrive.debug.js
@@ -3956,24 +3956,18 @@ function logoutWindowsLive(callback) {
         authServer = getAuthServerName(),
         path = "/oauth20_logout.srf?ts=";
     logoutFrame.src = "//" + authServer + path + new Date().getTime();
-    logoutFrame.onbeforeunload = function()
-    {
-        cleanLogoutFrame();
-        callback();
-    };
-
     document.body.appendChild(logoutFrame);
     wl_app.logoutFrame = logoutFrame;
 
     // Clean logout iframe and invoke callback once clear. Wait at most 30s.
-    //var start = new Date().getTime();
-    //var logoutWatcher = window.setInterval(function() {
-        //cleanLogoutFrame();
-        //if (wl_app.logoutFrame === null || (new Date()).getTime() - start > 30000) {
-          //  window.clearInterval(logoutWatcher);
-        //    callback();
-      //  }
-    //}, 1000 /* interval */);
+    var start = new Date().getTime();
+    var logoutWatcher = window.setInterval(function() {
+        if (wl_app.logoutFrame === null || (new Date()).getTime() - start > 30000) {
+            window.clearInterval(logoutWatcher);
+            cleanLogoutFrame();
+            callback();
+        }
+    }, 1000 /* interval */);
 }
 
 function cleanLogoutFrame() {
