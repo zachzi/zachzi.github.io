56ca87c8363571877a8bb3bb87eaacd90eb97578
Zachary Ziebarth <zachzi@live.com>
2015-10-07 16:03:19 -0700

    Update OneDriveSdk.js

--- OneDriveSdk.js
+++ OneDriveSdk.js
@@ -849,7 +849,7 @@ var PickerHelper = function () {
     }();
 module.exports = PickerHelper;
 },{"../Constants":1,"../Popup":5,"../models/PickerOptions":9,"./AccountChooserHelper":12,"./FilesV2Helper":15,"./Logging":16,"./ObjectHelper":17,"./RedirectHelper":19,"./VroomHelper":25}],19:[function(_dereq_,module,exports){
-var CallbackHelper = _dereq_('./CallbackHelper'), Constants = _dereq_('../Constants'), DomHelper = _dereq_('./DomHelper'), Logging = _dereq_('./Logging'), ObjectHelper = _dereq_('./ObjectHelper'), OneDriveState = _dereq_('../OneDriveState'), Popup = _dereq_('../Popup'), TypeValidationHelper = _dereq_('./TypeValidationHelper'), UrlHelper = _dereq_('./UrlHelper'), WindowStateHelper = _dereq_('./WindowStateHelper'), XHR = _dereq_('../XHR');
+var CallbackHelper = _dereq_('./CallbackHelper'), Constants = _dereq_('../Constants'), Logging = _dereq_('./Logging'), ObjectHelper = _dereq_('./ObjectHelper'), OneDriveState = _dereq_('../OneDriveState'), Popup = _dereq_('../Popup'), TypeValidationHelper = _dereq_('./TypeValidationHelper'), UrlHelper = _dereq_('./UrlHelper'), WindowStateHelper = _dereq_('./WindowStateHelper'), XHR = _dereq_('../XHR');
 var AAD_LOGIN_URL = 'https://login.microsoftonline.com/common/oauth2/authorize';
 var DISCOVERY_URL = 'https://onedrive.live.com/picker/businessurldiscovery';
 var RedirectHelper = function () {
@@ -996,19 +996,17 @@ var RedirectHelper = function () {
                     'z-index: 10000'
                 ];
             overlay.style.cssText = style.join(';');
-            DomHelper.onDocumentReady(function () {
-                var documentBody = document.body;
-                if (documentBody !== null) {
-                    documentBody.insertBefore(overlay, documentBody.firstChild);
-                } else {
-                    document.createElement('body').appendChild(overlay);
-                }
-            });
+            var documentBody = document.body;
+            if (documentBody !== null) {
+                documentBody.insertBefore(overlay, documentBody.firstChild);
+            } else {
+                document.createElement('body').appendChild(overlay);
+            }
         };
         return RedirectHelper;
     }();
 module.exports = RedirectHelper;
-},{"../Constants":1,"../OneDriveState":4,"../Popup":5,"../XHR":6,"./CallbackHelper":13,"./DomHelper":14,"./Logging":16,"./ObjectHelper":17,"./TypeValidationHelper":23,"./UrlHelper":24,"./WindowStateHelper":26}],20:[function(_dereq_,module,exports){
+},{"../Constants":1,"../OneDriveState":4,"../Popup":5,"../XHR":6,"./CallbackHelper":13,"./Logging":16,"./ObjectHelper":17,"./TypeValidationHelper":23,"./UrlHelper":24,"./WindowStateHelper":26}],20:[function(_dereq_,module,exports){
 var ApiEndpoint = _dereq_('../models/ApiEndpoint'), Constants = _dereq_('../Constants'), Logging = _dereq_('./Logging');
 var CID_PADDING = '0000000000000000';
 var CID_PADDING_LENGTH = CID_PADDING.length;
